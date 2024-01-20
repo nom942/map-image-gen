613607cd682a9878e983e2030f218f72df5571fa
ethleica@gmail.com <ethleica@gmail.com>
2024-01-21 01:08:41 +0800

    Added a way to manually upscale the img

--- MapImageGen/LogMap.cs
+++ MapImageGen/LogMap.cs
@@ -6,23 +6,52 @@ using System.IO;
 using System.Reflection.Emit;
 using UnityEngine;
 
-
 [HarmonyPatch(typeof(ImageGenerator), nameof(ImageGenerator.GenerateMap))]
 public static class LogMap
 {
     public static void MapCreated(Texture2D map)
     {
-        var bytearray = ImageConversion.EncodeToPNG(map); 
+        // added this resizer so you can just alter the desired width and height for the image
+        int newWidth = map.width * 8;  
+        int newHeight = map.height * 8; 
 
-        /*foreach (var color in map.colorMap)
+        Texture2D resizedMap = new Texture2D(newWidth, newHeight);
+
+        // get the pixels from the original map
+        Color[] originalPixels = map.GetPixels();
+
+        // apply scale factor for mapping pixels from original to resized map (so we can size the map up perfectly)
+        float xFactor = (float)map.width / newWidth;
+        float yFactor = (float)map.height / newHeight;
+
+        // loop through every pixel in the resized map
+        for (int x = 0; x < newWidth; x++)
         {
-            Log.Info($"{color.type} is {color.color}");
-        }*/
+            for (int y = 0; y < newHeight; y++)
+            {
+                // calculate each pixel coordinates in the original map
+                int originalX = Mathf.FloorToInt(x * xFactor);
+                int originalY = Mathf.FloorToInt(y * yFactor);
 
+                // get the colour from the original map and set it in the resized map (maybe we can allocate rooms different colours)
+                resizedMap.SetPixel(x, y, originalPixels[originalY * map.width + originalX]);
+            }
+        }
+
+        // apply changes to the resized map
+        resizedMap.Apply();
+
+        // encode the resized map to PNG
+        byte[] bytearray = ImageConversion.EncodeToPNG(resizedMap);
+
+        // specify the path for saving the image
         string path = Path.Combine("D:", $"Map{UnityEngine.Random.Range(0, 99999)}.png");
+
+        // log the path and save the image
         Exiled.API.Features.Log.Info(path);
         File.WriteAllBytes(path, bytearray);
     }
+
     public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
         List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
@@ -39,6 +68,5 @@ public static class LogMap
         {
             yield return instruction;
         }
-
     }
 }
